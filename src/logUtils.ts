cafe44de947561e4a6e28339b41a778e2a675f48
子清 <120438383+ziqing888@users.noreply.github.com>
2025-03-16 09:23:53 +0800

    Update logUtils.ts

--- src/logUtils.ts
+++ src/logUtils.ts
@@ -1,15 +1,12 @@
 import chalk from "chalk";
 import * as readline from "readline";
 
-
 const input = readline.createInterface({ input: process.stdin, output: process.stdout });
-
 
 export const askQuestion = (query: string): Promise<string> =>
   new Promise((resolve) => input.question(query, (ans) => resolve(ans)));
 
 export type LogType = "success" | "error" | "warning" | "debug" | "info";
-
 
 export function logInfo(account: number | null, total: number | null, msg: string, type: LogType = "info") {
   const time = new Date().toLocaleString("zh-CN", { hour12: false });
